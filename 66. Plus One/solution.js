22f15dd44e0e44dc3b4eb6d374aa55ec0f44404c
rathiharvir <rathiharvir@gmail.com>
2023-10-26 22:32:27 +0530

    66. Plus One

---
+++ 66. Plus One/solution.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let s = digits.join('')
+    let n = BigInt(s) + 1n
+    return n.toString().split('')
+};
